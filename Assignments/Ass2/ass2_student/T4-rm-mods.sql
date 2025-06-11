37a07262e8008153e46fe37f81366f8ed18c686c
Jichao Liang <jlia0084@student.monash.edu>
2025-06-11 14:29:02 +1000

    complete the task 4a

--- Assignments/Ass2/ass2_student/T4-rm-mods.sql
+++ Assignments/Ass2/ass2_student/T4-rm-mods.sql
@@ -12,8 +12,29 @@
 */
 
 --(a)
+SAVEPOINT before_add_column;
 
+ALTER TABLE competitor ADD comp_completed_events NUMBER(3);
+
+COMMENT ON COLUMN competitor.comp_completed_events IS 'Number of completed events for the competitor (finish time is not null)';
+
+UPDATE competitor 
+SET comp_completed_events = (
+    SELECT COUNT(*)
+    FROM entry
+    WHERE entry.comp_no = competitor.comp_no
+    AND entry.entry_finishtime IS NOT NULL
+);
+
+DESC competitor;
+
+SELECT comp_no, comp_fname, comp_lname, comp_completed_events
+FROM competitor
+ORDER BY comp_no;
+
+COMMIT;
 
 --(b)
+
 
 
