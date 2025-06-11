f9bcbe4da590070a71fb314aed037a62a82d22a1
Jichao Liang <jlia0084@student.monash.edu>
2025-06-11 17:03:26 +1000

    complete task5a and prepare report successfully

--- Assignments/Ass2/ass2_student/T5-rm-select.sql
+++ Assignments/Ass2/ass2_student/T5-rm-select.sql
@@ -19,6 +19,32 @@
 -- (;) at the end of this answer
 
 
+SELECT team.team_name,
+       team.carn_date,
+       competitor.comp_fname || ' ' || competitor.comp_lname AS teamleader,
+       (SELECT COUNT(*) 
+        FROM entry 
+        WHERE team_id = team.team_id) AS members
+FROM team
+JOIN entry ON team.event_id = entry.event_id AND team.entry_no = entry.entry_no
+JOIN competitor ON entry.comp_no = competitor.comp_no
+WHERE team.carn_date < TO_DATE('01/03/2025','DD/MM/YYYY')
+AND team.team_name IN (
+    SELECT team_name
+    FROM team
+    WHERE carn_date < TO_DATE('01/03/2025','DD/MM/YYYY')
+    GROUP BY team_name
+    HAVING COUNT(*) = (
+        SELECT MAX(team_count)
+        FROM (
+            SELECT COUNT(*) as team_count
+            FROM team
+            WHERE carn_date < TO_DATE('01/03/2025','DD/MM/YYYY')
+            GROUP BY team_name
+        )
+    )
+)
+ORDER BY team.team_name, team.carn_date;
 
 
 
